8c03d71b50c1ab4c80a1200bf7175629d9340682
SpenserScruggs <scru7556@vandals.uidaho.edu>
2022-11-03 22:09:03 -0700

    Sliders+Dropdown

--- WPF_GUI/MainWindow.xaml.cs
+++ WPF_GUI/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO.Ports;
 using System.Diagnostics;
+using System.Threading;
 
 namespace WPF_GUI
 {
@@ -22,17 +23,64 @@ namespace WPF_GUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private SerialPort port = new SerialPort("COM3", 9600);
+        static string portName;
+        private SerialPort port;
+        
 
         public MainWindow()
         {
             InitializeComponent();
             Debug.WriteLine("this is working");
         }
-        private void ExitButton_Click(object sender, RoutedEventArgs e)
+
+        private void SelectPort(object sender, SelectionChangedEventArgs e)
         {
-            this.Close();
+            portName = ((ComboBoxItem)ChosenPort.SelectedItem).Content.ToString();
+            Debug.WriteLine(portName);
+        }
+        
+        private void ConnectToPort(object sender, RoutedEventArgs e)
+        {
+            port = new SerialPort(portName, 9600);
+            try { 
+                port.Open();
+                Debug.WriteLine("opened port " + portName);
+                }
+            catch { }
+            Task.Delay(100);
+            Tracking();
         }
 
+        private void DisconnectFromPort(object sender, RoutedEventArgs e)
+        {
+            if (port.IsOpen)
+            {
+                port.Close();
+                Debug.WriteLine("port has been closed");
+            }
+        }
+
+        private async void Tracking()
+        {
+            while (true)
+            {
+                Debug.WriteLine(Size((int)Slider1.Value));
+                await Task.Delay(10);
+            }
+        }
+
+        private string Size(int x)
+        {
+            string output = x.ToString();
+            if(output.Length == 1)
+            {
+                output = "00" + output;
+            }
+            else if (output.Length == 2)
+            {
+                output = "0" + output;
+            }
+            return output;
+        }
     }
 }
