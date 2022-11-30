a0ce234c9ce6ce20f5e7ae5e06fbc7e668fdc6f7
SpenserScruggs <scru7556@vandals.uidaho.edu>
2022-11-29 18:00:29 -0800

    Add project files.

---
+++ RobotUI_UWP/CtrlPage.xaml.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Gaming.Input;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Gaming;
+using Windows.UI.Xaml.Documents;
+using Windows.System;
+using System.Diagnostics;
+using Windows.Networking;
+using System.IO.Ports;
+using System.ComponentModel.Design.Serialization;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RobotUI_UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    /// 
+
+    public sealed partial class CtrlPage : Page
+    {
+        static string portName2;
+        static bool printing2 = true;
+        SerialPort port2 = new SerialPort("COM1", 115200);
+        static double th1, th2, th3;
+        static double x = 0.0;
+        static double y = 0.0;
+        static double z = 0.0;
+        
+        private Gamepad _Gamepad = null;
+        public CtrlPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ReturnPage(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        
+
+        private async void btnConnect_Click(object sender,RoutedEventArgs e)
+        {
+            Gamepad.GamepadAdded += Gamepad_GamepadAdded;
+            Gamepad.GamepadRemoved += Gamepad_GamepadRemoved;
+
+            while (true)
+            {
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    if (_Gamepad == null)
+                    {
+                        return;
+                    }
+
+                    // Get the current state
+                    var reading = _Gamepad.GetCurrentReading();
+
+                    tbLeftTrigger.Text = Math.Round(reading.LeftTrigger, 1).ToString();
+                    tbRightTrigger.Text = Math.Round(reading.RightTrigger, 1).ToString();
+                    tbLeftThumbstickX.Text = Math.Round(reading.LeftThumbstickX, 1).ToString();
+                    if (Math.Round(reading.LeftThumbstickX, 1) * 0.01 + x < 2 && Math.Round(reading.LeftThumbstickX, 1) * 0.01 + x > 0)
+                    {
+                        x += Math.Round(reading.LeftThumbstickX, 1) * 0.01;
+                    }
+
+                    tbLeftThumbstickY.Text = Math.Round(reading.LeftThumbstickY, 1).ToString();
+                    if (Math.Round(reading.LeftThumbstickY, 1) * 0.01 + y < 2 && Math.Round(reading.LeftThumbstickY, 1) * 0.01 + y > 0)
+                    {
+                        y += Math.Round(reading.LeftThumbstickY, 1) * 0.01;
+                    }
+
+                    tbRightThumbstickX.Text = Math.Round(reading.RightThumbstickX, 1).ToString();
+                    if (Math.Round(reading.RightThumbstickX, 1) * 0.01 + z < 2 && Math.Round(reading.RightThumbstickX, 1) * 0.01 + z > 0)
+                    {
+                        z += Math.Round(reading.RightThumbstickX, 1) * 0.01;
+                    }
+
+                    tbRightThumbstickY.Text = Math.Round(reading.RightThumbstickY, 1).ToString();
+                    tbButtons.Text = string.Empty;
+                    tbButtons.Text += (reading.Buttons & GamepadButtons.A) == GamepadButtons.A ? "A " : "";
+                    tbButtons.Text += (reading.Buttons & GamepadButtons.B) == GamepadButtons.B ? "B " : "";
+                    tbButtons.Text += (reading.Buttons & GamepadButtons.X) == GamepadButtons.X ? "X " : "";
+                    tbButtons.Text += (reading.Buttons & GamepadButtons.Y) == GamepadButtons.Y ? "Y " : "";
+                    tbButtons.Text += (reading.Buttons & GamepadButtons.LeftShoulder) == GamepadButtons.LeftShoulder? "LeftShoulder " : "";
+                    tbButtons.Text += (reading.Buttons & GamepadButtons.RightShoulder) == GamepadButtons.RightShoulder? "RightShoulder " : "";
+                    tbButtons.Text += (reading.Buttons & GamepadButtons.LeftThumbstick) == GamepadButtons.LeftThumbstick? "LeftThumbstick " : "";
+                    tbButtons.Text += (reading.Buttons & GamepadButtons.RightThumbstick) == GamepadButtons.RightThumbstick? "RightThumbstick " : "";
+                    tbButtons.Text += (reading.Buttons & GamepadButtons.DPadLeft) == GamepadButtons.DPadLeft ? "DPadLeft " : "";
+                    tbButtons.Text += (reading.Buttons & GamepadButtons.DPadRight) == GamepadButtons.DPadRight ? "DPadRight " : "";
+                    tbButtons.Text += (reading.Buttons & GamepadButtons.DPadUp) == GamepadButtons.DPadUp ? "DPadUp " : "";
+                    tbButtons.Text += (reading.Buttons & GamepadButtons.DPadDown) == GamepadButtons.DPadDown ? "DPadDown " : "";});
+                Kinematics(x, y, z);
+                await Task.Delay(TimeSpan.FromMilliseconds(5));
+            }
+        }
+        private async void Gamepad_GamepadRemoved(object sender,Gamepad e)
+        {
+            _Gamepad = null;
+
+            await Dispatcher.RunAsync(
+                CoreDispatcherPriority.Normal, () =>
+                {
+                    tbConnected.Text = "Controller removed";
+                    Return.IsEnabled = true;
+                    btnConnect.IsEnabled = true;
+                });
+        }
+
+        private async void Gamepad_GamepadAdded(object sender,Gamepad e)
+        {
+            
+            _Gamepad = e;
+
+            await Dispatcher.RunAsync(
+            CoreDispatcherPriority.Normal, () =>
+            {
+                tbConnected.Text = "Controller added";
+                Return.IsEnabled = false;
+                btnConnect.IsEnabled = false;
+            });
+        }
+
+        private void SelectPort2(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                portName2 = ((ComboBoxItem)ChosenPort2.SelectedItem).Content.ToString();
+                Debug.WriteLine(portName2);
+            }
+            catch { }
+
+        }
+
+        private void ConnectToPort2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!port2.IsOpen)
+                {
+                    port2 = new SerialPort(portName2, 115200);
+                    port2.Open();
+                    Debug.WriteLine("opened port " + portName2);
+                }
+            }
+            catch { }
+            Task.Delay(100);
+            printing2 = true;
+            Tracking();
+        }
+
+        private void DisconnectFromPort2(object sender, RoutedEventArgs e)
+        {
+            printing2 = false;
+            if (port2.IsOpen)
+            {
+                port2.Close();
+                Debug.WriteLine("port has been closed");
+            }
+            
+        }
+
+        private async void Tracking()
+        {
+            while (printing2)
+            {
+                //Debug.WriteLine(Size((int)y));
+
+                try
+                {
+                    if (th1 > 0 && th2 > 0 && th3 > 0 && th1 < 180 && th2 < 180 && th3 < 180)
+                    {
+                        port2.WriteLine(Kinematics(x, y, z));
+                    }
+                }
+                catch { printing2 = false; }
+
+                await Task.Delay(10);
+            }
+        }
+
+        private string Size(int x)
+        {
+            string output = x.ToString();
+            if (output.Length == 1)
+            {
+                output = "00" + output;
+            }
+            else if (output.Length == 2)
+            {
+                output = "0" + output;
+            }
+            return output;
+        }
+
+        private string Kinematics(double x, double y, double z)
+        {
+            double s3, c3, k1, k2, A, B;
+            double l1 = 1;
+            double l2 = 1;
+            double l3 = 1;
+
+            th1 = Math.Atan2(y, x);
+            c3 = (x * x + y * y + z * z - (l1 * l1 + l2 * l2 + l3 * l3) - 2 * l1 * (z - l1)) / (2 * l2 * l3);
+            s3 = Math.Sqrt(1 - c3);
+
+            th3 = Math.Atan2(s3, c3);
+            k1 = c3 * l3 + l2;
+            k2 = s3 * l3;
+            A = -2 * k1 * (l1 - z);
+            B = Math.Pow(2 * k1 * (l1 - z), 2) - 4 * (k1 * k1 + k2 * k2) * (z * z + l1 * l1 - k2 * k2 - 2 * l1 * z);
+            th2 = Math.Asin((A + Math.Sqrt(B)) / (2 * (k1 * k1 + k2 * k2)));
+
+            th1 = th1 * 180 / Math.PI;
+            th2 = th2 * 180 / Math.PI;
+            th3 = th3 * 180 / Math.PI;
+
+            Debug.WriteLine(Size((int)th1) + Size((int)th2) + Size((int)th3) + "   " + x.ToString() + " " + y.ToString() + " " + z.ToString());
+            return Size((int)th1) + Size((int)th2) + Size((int)th3);
+        }
+    }
+}
---
+++ RobotUI_UWP/MainPage.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Gaming.Input;
+using Windows.Devices.Enumeration;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RobotUI_UWP
+{
+    public sealed partial class MainPage : Page
+    {
+        //Global Variables
+        static string portName;
+        static string themeName;
+        static bool printing = false;
+        private SerialPort port = new SerialPort("COM1", 115200);
+        public List<string> PosList = new List<string>();
+        static bool Recording = false;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = new Size(650, 500);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+
+        private void RecordPositions(object sender, RoutedEventArgs e)
+        {
+            if (Recording)
+            {
+                Recording = false;
+                Record.Opacity = 1;
+            }
+            else
+            {
+                PosList = new List<string>();
+                Recording = true;
+                Record.Opacity = 0.5;
+            }
+
+
+        }
+
+        private async void PlayRecording(object sender, RoutedEventArgs e)
+        {
+            printing = false;
+            Play.IsEnabled = false;
+            for (int i = 0; i < PosList.Count; i++)
+            {
+                try
+                {
+                    Debug.WriteLine(PosList[i]);
+                    //port.WriteLine(PosList[i]);
+                }
+                catch { }
+                await Task.Delay(10);
+
+            }
+            Play.IsEnabled = true;
+            printing = true;
+            Tracking();
+        }
+
+        private void ChangePage(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(CtrlPage));
+        }
+
+
+
+        private void SelectPort(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                portName = ((ComboBoxItem)ChosenPort.SelectedItem).Content.ToString();
+                Debug.WriteLine(portName);
+            }
+            catch { }
+
+        }
+
+        private void ConnectToPort(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!port.IsOpen)
+                {
+                    port = new SerialPort(portName, 115200);
+                    port.Open();
+                    Debug.WriteLine("opened port " + portName);
+                }
+            }
+            catch { }
+            Task.Delay(100);
+            printing = true;
+            Tracking();
+        }
+
+        private void CtrlConnect(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DisconnectFromPort(object sender, RoutedEventArgs e)
+        {
+            if (port.IsOpen)
+            {
+                port.Close();
+                Debug.WriteLine("port has been closed");
+            }
+            printing = false;
+        }
+
+        private async void Tracking()
+        {
+            while (printing)
+            {
+                Debug.WriteLine(Size((int)Slider1.Value) + Size((int)Slider2.Value) + Size((int)Slider3.Value) +
+                    Size((int)Slider4.Value) + Size((int)Slider5.Value) + Size((int)Slider6.Value));
+
+                try
+                {
+                    port.WriteLine(Size((int)Slider1.Value) + Size((int)Slider2.Value) + Size((int)Slider3.Value) +
+                    Size((int)Slider4.Value) + Size((int)Slider5.Value) + Size((int)Slider6.Value));
+                }
+                catch { printing = false; }
+
+                await Task.Delay(10);
+            }
+        }
+
+        private async void SavePos()
+        {
+            await Task.Delay(10);
+        }
+
+        private string Size(int x)
+        {
+            string output = x.ToString();
+            if (output.Length == 1)
+            {
+                output = "00" + output;
+            }
+            else if (output.Length == 2)
+            {
+                output = "0" + output;
+            }
+            return output;
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
